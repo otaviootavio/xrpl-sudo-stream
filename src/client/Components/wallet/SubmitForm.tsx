6478ef4387cf1be9fafb974ab27b7e2542ef6cf0
otaviootavio <otaviovacari@usp.br>
2023-10-24 12:29:53 -0300

    fix fetch

--- src/client/Components/wallet/SubmitForm.tsx
+++ src/client/Components/wallet/SubmitForm.tsx
@@ -1,16 +1,18 @@
 import React, { useState } from "react";
 import { decode } from "xrpl";
 
-
 const SubmitForm = () => {
   const [submitResponse, setSubmitResponse] = useState<string>("");
-  const [txBlob, setTxBlob] = useState<string>("")
+  const [txBlob, setTxBlob] = useState<string>("");
   const [isLoading, setIsLoading] = useState(false);
 
   const submitSignedTransaction = async () => {
     try {
-      setIsLoading(true)
-      const response = await fetch("http://localhost:3000/wallet/submit", {
+      setIsLoading(true);
+      const currentPath: Location = window.location;
+      const currentUrl: URL = new URL(currentPath.href + "wallet/submit");
+
+      const response = await fetch(currentUrl.href, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
@@ -21,7 +23,7 @@ const SubmitForm = () => {
       const result = await response.json();
 
       setSubmitResponse(JSON.stringify(result, null, 2));
-      setIsLoading(false)
+      setIsLoading(false);
     } catch (error) {
       console.error("Error submitting transaction:", error);
     }
@@ -35,9 +37,18 @@ const SubmitForm = () => {
       className="my-5"
     >
       <label>
-        tx_blob: <input type="text" value={txBlob} onChange={(e) => {setTxBlob(e.target.value)}} />
+        tx_blob:{" "}
+        <input
+          type="text"
+          value={txBlob}
+          onChange={(e) => {
+            setTxBlob(e.target.value);
+          }}
+        />
       </label>
-      <button disabled={isLoading} onClick={submitSignedTransaction}>{isLoading ? "Loading..." : "Submit"}</button>
+      <button disabled={isLoading} onClick={submitSignedTransaction}>
+        {isLoading ? "Loading..." : "Submit"}
+      </button>
       <textarea
         style={{
           height: "1000px",
