c0f4289b510a3df78bba34e44bd2f9f087bfaf4e
otaviootavio <otaviovacari@usp.br>
2023-11-14 12:27:37 -0300

    auth at routes

--- src/server/main.ts
+++ src/server/main.ts
@@ -4,13 +4,14 @@ import bodyParser from "body-parser";
 import { helloController } from "./controllers/helloController";
 import walletRoutes from "./routes/walletRoutes";
 import userRoutes from "./routes/userRoutes";
+import { authMiddleware } from "./auth-middleware";
 
 const app = express();
 app.use(bodyParser.json());
 
 app.get("/hello", helloController);
 app.use('/wallet', walletRoutes);
-app.use('/', userRoutes);
+app.use('/users', authMiddleware, userRoutes);
 
 ViteExpress.listen(app, 3000, () =>
   console.log("Server is listening on port 3000...")
--- src/server/routes/userRoutes.ts
+++ src/server/routes/userRoutes.ts
@@ -1,18 +1,18 @@
-import express from 'express';
-import { userController } from '../controllers/userController';
-import { authMiddleware } from '../auth-middleware';
+import express from "express";
+import { userController } from "../controllers/userController";
+import { authMiddleware } from "../auth-middleware";
 
 const userRoutes = express.Router();
 
-userRoutes.post('/users', authMiddleware, userController.createNewUser);
+userRoutes.post("/", authMiddleware, userController.createNewUser);
 // userRoutes.put('/users/:uid', authMiddleware, userController.updateUser);
 
-userRoutes.put('/users/:uid/wallets', authMiddleware, userController.addWalletsToUser);
-userRoutes.get('/users/:uid/wallets', authMiddleware, userController.getWalletsFromUser);
-userRoutes.delete('/users/:uid/wallets', authMiddleware, userController.deleteWalletsFromUser);
+userRoutes.put("/:uid/wallets", userController.addWalletsToUser);
+userRoutes.get("/:uid/wallets", userController.getWalletsFromUser);
+userRoutes.delete("/:uid/wallets", userController.deleteWalletsFromUser);
 
-userRoutes.delete('/users/:uid', authMiddleware, userController.deleteUser);
-userRoutes.get('/users/:uid', authMiddleware, userController.getUserByUID);
-userRoutes.get('/users', userController.listAllUsers);
+userRoutes.delete("/:uid", userController.deleteUser);
+userRoutes.get("/:uid", userController.getUserByUID);
+userRoutes.get("/", userController.listAllUsers);
 
 export default userRoutes;
