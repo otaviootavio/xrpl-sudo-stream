126d77a3922bbfc85eacc260b7709c10fb40139f
otaviootavio <otaviovacari@usp.br>
2023-12-02 00:28:39 -0300

    update to accout number

--- src/client/Components/wallet/Identicon.tsx
+++ src/client/Components/wallet/Identicon.tsx
@@ -1,8 +1,8 @@
 import React, { Dispatch, SetStateAction, useEffect, useRef } from "react";
-import PublicKeyDisplay from "./PublicKeyDisplay";
+import PublicHexDisplay from "./PublicHexDisplay";
 
 interface IdenticonProps {
-  publicKey: string;
+  classicAddress: string;
   setCurrentWallet: Dispatch<SetStateAction<WalletDataType | null>>;
   size?: number;
   thisWallet: WalletDataType;
@@ -11,22 +11,22 @@ interface IdenticonProps {
 const Identicon: React.FC<IdenticonProps> = ({
   thisWallet,
   setCurrentWallet,
-  publicKey,
+  classicAddress,
   size = 64,
 }) => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
-  const getColorFromPublicKey = (publicKey: string): string => {
-    const colorCode = publicKey.slice(-3);
+  const getColorFromPublicKey = (classicAddress: string): string => {
+    const colorCode = classicAddress.slice(-3);
     return `#${colorCode.padEnd(6, "0")}`;
   };
 
-  const color = getColorFromPublicKey(publicKey);
+  const color = getColorFromPublicKey(classicAddress);
 
   return (
     <div>
-      <span onClick={() => setCurrentWallet(thisWallet)} style={{ marginRight: "10px", cursor: "pointer" }} >{publicKey.slice(0, 6)}...</span>
-      <PublicKeyDisplay publicKey={publicKey} />
+      <span onClick={() => setCurrentWallet(thisWallet)} style={{ marginRight: "10px", cursor: "pointer" }} >{classicAddress.slice(0, 6)}...</span>
+      <PublicHexDisplay publicHex={classicAddress} />
     </div>
   );
 };
